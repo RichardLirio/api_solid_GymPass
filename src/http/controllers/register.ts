7f6da17d4a87f96dbdab0097c3b5e7ffd0d9c3e9
Richard Lirio <richardlirio@hotmail.com>
2025-02-18 10:42:10 -0300

    feat:implements global errors

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -2,6 +2,7 @@ import { z } from "zod";
 import { FastifyRequest, FastifyReply } from "fastify";
 import { RegisterUseCase } from "@/use-cases/register";
 import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository";
+import { UserAlreadyExistsError } from "@/use-cases/erros/user-already-exists-error";
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -18,7 +19,13 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
     await registerUseCase.execute({ name, email, password }); //executo o metodo execute criado dentro da classe do use case
   } catch (error) {
-    return reply.status(409).send();
+    if (error instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({
+        message: error.message,
+      });
+    }
+
+    return reply.status(500).send(); //TODO: fix me
   }
 
   return reply.status(201).send();
