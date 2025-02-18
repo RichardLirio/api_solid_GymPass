382dfee4ff5271bb21b64dba3fe6fb048e305b95
Richard Lirio <richardlirio@hotmail.com>
2025-02-18 10:11:13 -0300

    chore:hash password and dependency inversion principle implements

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,6 +1,7 @@
-import { prisma } from "@/lib/prisma";
 import { z } from "zod";
 import { FastifyRequest, FastifyReply } from "fastify";
+import { RegisterUseCase } from "@/use-cases/register";
+import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository";
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -11,13 +12,14 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
   const { name, email, password } = registerBodySchema.parse(request.body);
 
-  await prisma.user.create({
-    data: {
-      name,
-      email,
-      password_hash: password,
-    },
-  });
+  try {
+    const usersRepository = new PrismaUsersRepository(); //crio o repository que será usado na classe do use case
+    const registerUseCase = new RegisterUseCase(usersRepository); //instacio o use case com o repository
+
+    await registerUseCase.execute({ name, email, password }); //executo o metodo execute criado dentro da classe do use case
+  } catch (error) {
+    return reply.status(409).send();
+  }
 
   return reply.status(201).send();
 }
