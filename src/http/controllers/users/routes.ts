da5ea9afd4fb71f8d5e006e9ebd108acf277187c
Richard Lirio <richardlirio@hotmail.com>
2025-03-18 21:27:47 -0300

    chore: implemented swagger documentation

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -6,7 +6,51 @@ import { VerifyJWT } from "@/http/middlewares/verify-jwt";
 import { refresh } from "./refresh";
 
 export async function usersRoutes(app: FastifyInstance) {
-  app.post("/users", register);
+  app.post(
+    "/users",
+    {
+      schema: {
+        summary: "Register a new user.",
+        description: "Register a new user in Database",
+        tags: ["Users"],
+        body: {
+          type: "object",
+          properties: {
+            name: { type: "string", description: "User Name" },
+            email: {
+              type: "string",
+              format: "email",
+              description: "User E-mail",
+            },
+            password: { type: "string", description: "USer Password" },
+          },
+          required: ["name", "email", "password"],
+          examples: [
+            {
+              name: "João Silva",
+              email: "joao.silva@example.com",
+              password: "senha123",
+            },
+          ],
+        },
+        response: {
+          201: {
+            description: "User successfuly register.",
+            type: "object",
+            properties: {},
+          },
+          409: {
+            description: "E-mail already exists",
+            type: "object",
+            properties: {
+              message: { type: "string" },
+            },
+          },
+        },
+      },
+    },
+    register
+  );
   app.post("/sessions", autheticate);
 
   app.patch("/token/refresh", refresh);
